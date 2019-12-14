3dcd2ea9bb6ce3a82e0b27a29543eeab88dd00bc
Nnabueze Uhiara <nuhiara@opay.team>
2019-12-14 10:03:50 +0100

    Primary call button no longer launching call intent. #10

--- app/src/main/java/com/nezspencer/callpolice/MainViewModel.kt
+++ app/src/main/java/com/nezspencer/callpolice/MainViewModel.kt
@@ -2,9 +2,11 @@ package com.nezspencer.callpolice
 
 import android.content.Context
 import android.os.Looper
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.firebase.database.DatabaseReference
+import java.util.*
 
 class MainViewModel(
     context: Context,
@@ -12,6 +14,30 @@ class MainViewModel(
     fusedLocationProviderClient: FusedLocationProviderClient,
     mainLooper: Looper
 ) : ViewModel() {
+    var userState: String? = null
+    val contactList = mutableListOf<ContactByState>()
+    var stateContact: ContactByState? = null
     val firebaseLiveData = FirebaseLiveData(currentNode)
     val locationLiveData = LocationLivedata(context, fusedLocationProviderClient, mainLooper)
+    val locationContactsMediator = MediatorLiveData<Boolean>().apply {
+        addSource(firebaseLiveData) {
+            it ?: return@addSource
+            contactList.clear()
+            contactList.addAll(it)
+            value =
+                !firebaseLiveData.value.isNullOrEmpty() && !locationLiveData.value.isNullOrBlank()
+        }
+        addSource(locationLiveData) {
+            it ?: return@addSource
+            userState = it
+            value =
+                !firebaseLiveData.value.isNullOrEmpty() && !locationLiveData.value.isNullOrBlank()
+        }
+    }
+
+    fun findPhoneNumbersForUserState(state: String): ContactByState? {
+        return contactList.find {
+            it.state.toLowerCase(Locale.ENGLISH) == state.toLowerCase(Locale.ENGLISH)
+        }
+    }
 }
